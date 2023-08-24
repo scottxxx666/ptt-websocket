819e5bcbf2ce63525816d9fbc5c7f6186ed55ec2
Scott Kao <scottxxx666@gmail.com>
2023-08-24 23:33:14 +0800

    feat: push message

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"os"
-	"strconv"
 	"syscall/js"
 	"time"
 )
@@ -13,6 +11,7 @@ import (
 func main() {
 	done := make(chan int, 0)
 	js.Global().Set("pollingMessages", js.FuncOf(PollingMessagesJs))
+	js.Global().Set("pushMessage", js.FuncOf(PushMessagesJs))
 	<-done
 }
 
@@ -22,88 +21,38 @@ func PollingMessagesJs(this js.Value, args []js.Value) interface{} {
 	return nil
 }
 
-func TryPushAndPull(account string, password string, revoke bool, board string, article string) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-	defer cancel()
-
-	ptt := NewPttClient(ctx)
-	err := ptt.Connect()
-	if err != nil {
-		return
+func PushMessage(message string) string {
+	if ptt == nil {
+		panic("not connect ptt yet")
 	}
-	defer ptt.Close()
-
-	err = ptt.Login(os.Getenv("account"), os.Getenv("password"), false)
-	if err != nil {
-		if errors.Is(err, AuthError) {
-			fmt.Println("密碼不對或無此帳號")
-		}
-		return
-	}
-
-	i := 1
-	go func() {
-		time.Sleep(3 * time.Second)
-		for {
-			err = ptt.PushMessage(strconv.Itoa(i))
-			i += 1
-			if err != nil {
-				fmt.Println(err)
-			}
-			time.Sleep(1 * time.Second)
-		}
-	}()
-
-	go func() {
-		err = ptt.PullMessages(board, article)
-		if err != nil {
-			if errors.Is(err, WrongArticleIdError) {
-				fmt.Println("找不到這個文章代碼(AID)，可能是文章已消失，或是你找錯看板了")
-			}
-			return
-		}
-	}()
-
-	for {
-
-	}
-}
-
-func PushMessage(account string, password string, board string, article string, message string) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-	defer cancel()
-
-	ptt := NewPttClient(ctx)
-	err := ptt.Connect()
-	if err != nil {
-		return
-	}
-	defer ptt.Close()
-
-	err = ptt.Login(account, password, false)
-	if err != nil {
-		if errors.Is(err, AuthError) {
-			fmt.Println("密碼不對或無此帳號")
-		}
-		return
-	}
-
-	err = ptt.PushMessage(message)
+	err := ptt.PushMessage(message)
 	if err != nil {
 		fmt.Println(err)
+		return "ERRR"
 	}
+	return ""
+}
+
+func PushMessagesJs(this js.Value, args []js.Value) interface{} {
+	var res string
+	go func() {
+		res = PushMessage(args[0].String())
+	}()
+	return res
 }
 
 func logError(msg string, e error) {
 	fmt.Println(msg, e)
 }
 
+var ptt *PttClient
+
 func PollingMessages(account string, password string, revokeOthers bool, board string, article string,
 	callback js.Value, reject js.Value) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
-	ptt := NewPttClient(ctx)
+	ptt = NewPttClient(ctx)
 	err := ptt.Connect()
 	if err != nil {
 		return
